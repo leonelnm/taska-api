71bf769855cb21dddeabb147dc6d657b62cccfec
leonelnm <elerickmontenegro@gmail.com>
2025-09-01 13:04:33 +0200

    Remove unused Spring Security dependency

--- src/main/java/com/codigozerocuatro/taska/infra/config/security/AuthConfig.java
+++ src/main/java/com/codigozerocuatro/taska/infra/config/security/AuthConfig.java
@@ -3,7 +3,6 @@ package com.codigozerocuatro.taska.infra.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
